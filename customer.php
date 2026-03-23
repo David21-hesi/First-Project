073c39e71fd2848f6e34f23f04228c0f4b946637
David <dforrosuelo847@gmail.com>
2026-03-23 14:14:38 +0800

    added php feature

---
+++ customer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Basics</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <?php
+    echo "Hello, World!"; // Outputs text to the browser
+    ?>
+</body>
+</html>
